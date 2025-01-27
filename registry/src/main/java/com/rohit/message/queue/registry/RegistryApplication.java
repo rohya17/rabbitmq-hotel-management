ea51561ef9028a78bb7c259bb1064f6694ce3ccc
ROHIT\DELL <thoraverohit17895@gmail.com>
2025-01-27 22:49:49 +0530

    mail and sms server

--- registry/src/main/java/com/rohit/message/queue/registry/RegistryApplication.java
+++ registry/src/main/java/com/rohit/message/queue/registry/RegistryApplication.java
@@ -2,8 +2,10 @@ package com.rohit.message.queue.registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class RegistryApplication {
 
 	public static void main(String[] args) {
---
+++ sms-server/src/main/java/com/rohit/message/queue/sms_server/model/SmsHistory.java
@@ -0,0 +1,5 @@
+package com.rohit.message.queue.sms_server.model;
+
+public class SmsHistory {
+
+}
